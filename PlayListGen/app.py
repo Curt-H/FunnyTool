f2b7d84c44a02c596c2e7579bd6cc4fb747eb6ca
Curt <Guardian.curt@outlook.com>
2019-04-05 02:31:57 +0800

    Feat: pls file generator

---
+++ PlayListGen/app.py
@@ -0,0 +1,33 @@
+def load_list():
+    with open("list.txt", mode="r", encoding="utf-8") as f:
+        names = f.readlines()
+    return names
+
+
+def generate_content(names):
+    ns = names
+    counter = 0
+    files = ""
+    filetype = "." + input("Please input filetype\n>>>: ")
+
+    for n in ns:
+        n = n.strip()
+        if len(n) > 0:
+            counter += 1
+            n += filetype
+            files += f"File{counter}={n}\n"
+
+    content = f"[playlist]\nNumberOfEntries={counter}\n" + files
+
+    return content
+
+
+def dump_pls(content):
+    with open("playlist.pls", mode="w", encoding="utf-8") as f:
+        f.write(content)
+
+
+if __name__ == '__main__':
+    names = load_list()
+    content = generate_content(names)
+    dump_pls(content)
