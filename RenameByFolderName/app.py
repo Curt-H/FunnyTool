da379c170b77e4c56d802399d46bc3ba3f327875
Curt <Guardian.curt@outlook.com>
2019-07-23 21:56:05 +0800

    Update app.py

--- RenameByFolderName/app.py
+++ RenameByFolderName/app.py
@@ -22,11 +22,11 @@ if __name__ == '__main__':
             filetype = os.path.splitext(file)[-1]
             new_fname_main = folder.split('\\')[-1]
             new_fname = new_fname_main + add_to_3_digit(index) + filetype
-            new_folder = os.path.join(*folder.split('\\')[:-1])
+            new_folder = path
             print(new_fname)
 
             old_path = os.path.join(folder, file)
-            new_path = os.path.join(new_folder, new_fname).replace(':',':\\')
+            new_path = os.path.join(new_folder, new_fname)
             print(old_path)
             print(new_path)
             os.rename(old_path, new_path)
