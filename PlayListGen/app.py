0d2859df7a4a59d40cebd247ddac4e45595c48cc
FFF000 <guardian.curt@outlook.com>
2019-08-28 23:09:17 +0800

    Feat: Refactor Playlistgen

--- PlayListGen/app.py
+++ PlayListGen/app.py
@@ -1,7 +1,34 @@
-def load_list():
-    with open("list.txt", mode="r", encoding="utf-8") as f:
-        names = f.readlines()
-    return names
+import os
+
+mediatail = [
+    '.mp4',
+    '.mkv',
+    '.ts',
+    '.rmvb',
+    '.avi',
+    '.flv',
+    '.mpg',
+    '.mpeg',
+    '.m4v',
+]
+
+
+def get_video_names(dir_path):
+    medialist = list()
+    for path, directories, files in os.walk(dir_path):
+        for f in files:
+            mediapath = os.path.join(path, f)
+            # Check the tail if it is a media file
+            tail = os.path.splitext(mediapath)[1].lower()
+            if tail in mediatail:
+                medialist.append(mediapath)
+    # Convert abs path to relative path
+    # Do not us dir_path, because "D:\\" in dir_path is "D:\"
+    # so it will not work properly
+    common_path = os.path.commonprefix(medialist)
+    for i in range(len(medialist)):
+        medialist[i] = medialist[i].replace(common_path, "")
+    return medialist
 
 
 def generate_content(names):
@@ -22,13 +49,19 @@ def generate_content(names):
     return content
 
 
-def dump_pls(content):
-    with open("playlist.pls", mode="w", encoding="utf-8") as f:
+def dump_pls(path, content):
+    with open(f"{path}\\playlist.pls", mode="w", encoding="utf-8") as f:
         f.write(content)
 
 
 if __name__ == '__main__':
-    names = load_list()
-    content = generate_content(names)
-    dump_pls(content)
-    input("Fin")
+    while True:
+        work_path = input("Please input the path:\n")
+        media_name_list = get_video_names(work_path)
+        contents = generate_content(media_name_list)
+        dump_pls(work_path, contents)
+        print("Generate pls successfully")
+
+        quit_app = input("Do it again? input q to quit!\n")
+        if quit_app == 'q' or quit_app == "Q":
+            exit(0)
